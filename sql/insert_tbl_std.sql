aaa590fd181ad0df004d7d6636778c46744cf532
nagakuray <naruhodo.u1@icloud.com>
2016-05-21 22:02:51 +0900

    sql file update

---
+++ sql/insert_tbl_std.sql
@@ -0,0 +1,3 @@
+insert into tbl_std(id,name,eng_name,entrance,addr1,addr2) values('00001','砂糖太郎','Sato Taro','2000-04-01','北海道','1丁目')
+insert into tbl_std(id,name,eng_name,entrance,addr1,addr2) values('00002','ウルトラマン','Urutora Man','1998-04-01','北海道','1丁目')
+insert into tbl_std(id,name,eng_name,entrance,addr1,addr2) values('00005','ライダー1号','Raider One','2002-01-01','畑','2丁目')
