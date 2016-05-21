2ab3b9be2f9895abda43d0445cdd03b2deb9e691
nagakuray <naruhodo.u1@icloud.com>
2016-05-21 22:59:42 +0900

    update sql files

--- sql/insert_tbl_exam.sql
+++ sql/insert_tbl_exam.sql
@@ -1,4 +1,4 @@
-insert into tbl_exam(id, eng, math) values('00001','80','85')
-insert into tbl_exam(id, eng, math) values('00002','70','95')
-insert into tbl_exam(id, eng, math) values('00004','90','55')
+insert into tbl_exam(id, eng, math) values('00001','80','85');
+insert into tbl_exam(id, eng, math) values('00002','70','95');
+insert into tbl_exam(id, eng, math) values('00004','90','55');
 
